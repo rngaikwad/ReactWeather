d98d2c823188b5395c04106196d62481fc20fa4c
rngaikwad <rnggaikwad@gmail.com>
2017-03-03 16:02:36 +0530

    initial commit

---
+++ app/components/About.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var About = React.createClass({
+	render : function () {
+
+		return(
+				<h1> about component!</h1>
+			);
+	}
+
+});
+
+module.exports = About;
---
+++ example-arrow-functions.js
@@ -0,0 +1,14 @@
+// var names = ['Andrew','Julie','jen'];
+
+// names.forEach(function(name){
+
+// 	console.log('forEeach',name);
+// });
+
+// names.forEach((name)=>{
+// 	console.log('arrowFunc',name);
+// });
+// names.forEach((name)=> console.log(name));
+
+var returnMe = (name)=>name+'!';
+console.log(returnMe('Andrew'));
