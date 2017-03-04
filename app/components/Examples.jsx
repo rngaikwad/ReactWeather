db8d06515c68080ba0c221c1dfc415e8ffdbe9d7
raghu <raghu>
2017-03-04 13:34:07 -0800

    added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,7 +4,10 @@ var React = require('react');
 var Examples = (props) => {
 
 		return (
+			<div>
 				<h3>Examples component!</h3>
+				<p> Welcome to example component</p>
+			</div>
 			);
 
 };
