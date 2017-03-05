bd6018479ca511c7567009b075cd49ce6cbadda6
raghu <raghu>
2017-03-04 17:45:36 -0800

    new heroku

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ var app =  express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(function (req, res, next) {
-	if(req.headers['x-forwarded-proto'] === 'http'){
-		next();
+// app.use(function (req, res, next) {
+// 	if(req.headers['x-forwarded-proto'] === 'http'){
+// 		next();
 
-	} else{
+// 	} else{
 
-		res.redirect('http://' + req.hostname + req.url);
+// 		res.redirect('http://' + req.hostname + req.url);
 
-	}
-});
+// 	}
+// });
 
 app.use(express.static('public'));
 
